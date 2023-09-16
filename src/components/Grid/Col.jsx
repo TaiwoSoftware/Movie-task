607475febed69abc8e214acd0bb2d488e45f8b57
Taiwo AKerele <davcodes24@gmail.com>
2023-09-16 20:54:21 +0100

    favicon

--- src/components/Grid/Col.jsx
+++ src/components/Grid/Col.jsx
@@ -4,22 +4,15 @@ const Col = ({
   alt,
   headerOne,
   special,
-  logo,
-  tomatoes,
-  type,
-  adventure,
+ 
   release
 }) => {
   return (
     <div className="col col-md-4" data-testid="Movie-card">
       <img src={src} alt={alt}  data-testid="movie-poster"/>
       <h3 className={special} data-testid="movie-title" >{headerOne}</h3>
-      <div className="inner-logo">
-        <img src={logo} alt="copied-logo" className="logo-copy" />
-        <img src={tomatoes} alt="tomotoes" className="tomotoes-copy" />
-      </div>
+
       <p data-testid="release-date" className="release">{release}</p>
-      <p className={adventure}>{type}</p>
     </div>
   );
 };
--- src/components/Grid/LastRow.jsx
+++ src/components/Grid/LastRow.jsx
@@ -1,7 +1,5 @@
 import Col from "./Col";
 import image1 from "../poster-cards/rage.jpg";
-import ImbdLogo from "../images/IMDB - Copy.svg";
-import Rotten from "../images/Rotten Tomatoes - Copy.svg";
 const LastRow = () => {
   return (
     <div className="row lastRow">
@@ -10,10 +8,7 @@ const LastRow = () => {
         alt="image1"
         headerOne="I Am Rage"
         special="firstimagestyle"
-        logo={ImbdLogo}
-        tomatoes={Rotten}
-        type="Horror"
-        adventure="changing"
+        
         release="2023-09-14"
       />
       <div className="fix"></div>
