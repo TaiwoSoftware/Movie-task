778c3f274375a6af03382047f57a3c13f69dbb3f
Taiwo AKerele <davcodes24@gmail.com>
2023-09-16 20:18:25 +0100

    navbar

--- src/components/Header/Main/MainPart.jsx
+++ src/components/Header/Main/MainPart.jsx
@@ -4,27 +4,26 @@ import ImbdLogo from "./ImbdLogo";
 import RottenLogo from "./RottenLogo";
 
 const MainPart = () => {
-  // const apiKey =
-  //   "https://api.themoviedb.org/3/discover/movie?api_key=78bcd9519ba51e4395c843281cef3d37";
-  // const result = async () => {
-  //   try {
-  //     const res = await fetch(apiKey);
-  //     const data = await res.json();
+  const apiKey =
+    "https://api.themoviedb.org/3/discover/movie?api_key=78bcd9519ba51e4395c843281cef3d37";
+  const result = async () => {
+    try {
+      const res = await fetch(apiKey);
+      const data = await res.json();
 
-  //     console.log(data);
-  //     const { results } = data;
-  //     const mappedResult = results.map((result) => {
-  //       const { backdrop_path, title } = result;
-  //       console.log(backdrop_path);
-  //       console.log(title);
-  //     });
-  //     mappedResult();
-  //   } catch (error) {
-  //     console.log(error, "Error");
-  //   }
-  // };
+      console.log(data);
+      const { results } = data;
+      const mappedResult = results.map((result) => {
+        const { backdrop_path, title } = result;
+        console.log(backdrop_path);
+        console.log(title);
+      });
+    } catch (error) {
+      alert(error, "Error");
+    }
+  };
 
-  // result();
+  result();
   return (
     <div>
       <div className="main-part">
